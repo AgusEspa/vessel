3b1fb0566764104c240bf560f8f246a2038decf3
Agustin Espana <agustinespania@gmail.com>
2022-05-17 09:30:32 -0300

    added navbar audio

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,61 @@
 import { useState } from "react";
-import styles from "../styles/Main.module.scss";
+import styles from "../styles/Navbar.module.scss";
 
 const Navbar = () => {
 	const [toggleMenu, setToggleMenu] = useState(false);
+	const [toggleAudio, setToggleAudio] = useState(false);
 
 	const handleToggleMenu = (event) => {
 		event.preventDefault();
 		setToggleMenu((prevState) => !prevState);
 	};
 
+	const handleToggleAudio = (event) => {
+		event.preventDefault();
+		setToggleAudio((prevState) => !prevState);
+	};
+
 	return (
-		<nav className={styles.navbarContainer}>
-			<div className={styles.helperIcons}>
-				<div>
-					<audio src="mystique-16608.mp3" autoPlay controls />
+		<>
+			<nav className={styles.navbarContainer}>
+				<div className={styles.helperIcons}>
+					<button onClick={handleToggleAudio}>
+						<p>A</p>
+					</button>
 				</div>
+				<div className={styles.navLogoBox}>
+					<img
+						className={styles.navLogo}
+						src="logo-reduced.png"
+						alt="logo"
+					/>
+				</div>
+				<div className={styles.helperIcons}>
+					<button
+						className={
+							!toggleMenu
+								? styles.hamburger
+								: styles.hamburgerActive
+						}
+						onClick={handleToggleMenu}
+					>
+						<span className={styles.bar}></span>
+						<span className={styles.bar}></span>
+						<span className={styles.bar}></span>
+					</button>
+				</div>
+			</nav>
+
+			<div
+				className={
+					!toggleAudio
+						? styles.audioContainer
+						: styles.audioContainerActive
+				}
+			>
+				<audio src="mystique-16608.mp3" autoPlay controls />
 			</div>
-			<div className={styles.navLogoBox}>
-				<img
-					className={styles.navLogo}
-					src="logo-reduced.png"
-					alt="logo"
-				/>
-			</div>
-			<div className={styles.helperIcons}>
-				<button
-					className={
-						!toggleMenu ? styles.hamburger : styles.hamburgerActive
-					}
-					onClick={handleToggleMenu}
-				>
-					<span className={styles.bar}></span>
-					<span className={styles.bar}></span>
-					<span className={styles.bar}></span>
-				</button>
-			</div>
-		</nav>
+		</>
 	);
 };
 
