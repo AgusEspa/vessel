96b8ac3c8997c85f283d87f447f10d94b3bd3346
Agustin Espana <agustinespania@gmail.com>
2022-05-17 10:37:20 -0300

    Update Landing.js

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,7 +1,7 @@
 import styles from "../styles/Main.module.scss";
 
 const Landing = (props) => {
-	return <div className={styles.landingContainer}>a</div>;
+	return <div className={styles.landingContainer}></div>;
 };
 
 export default Landing;
