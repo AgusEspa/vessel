8bd799e68c5f80634bde435dba5dbb93b8c4cf56
Agustin Espana <agustinespania@gmail.com>
2022-05-17 23:07:29 -0300

    fixed background position

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -28,32 +28,38 @@ const Landing = (props) => {
 			<div className={styles.welcomeContainer}>
 				<div>
 					<p>
-						Lorem ipsum is placeholder text commonly used in the
-						graphic, print, and publishing industries for previewing
-						layouts and visual mockups.
+						Vessel exists as a layered construct only temporarily
+						through the virtual world. It’s a remnant of an obscure
+						programming technique to provide random access to an
+						individual’s identity graph, a processing network not
+						too distant from a human brain, albeit less complex. And
+						like a brain, it has a conditioned stability: elements
+						change their size, their interoperability, even their
+						references.
 					</p>
 					<p>
-						Lorem ipsum is placeholder text commonly used in the
-						graphic, print, and publishing industries for previewing
-						layouts and visual mockups.
+						We know nothing about you, just one thing, one thing
+						that you don’t know about yourself. You could put down
+						your headset and start over —it could be just a
+						malfunction after all— or it could be the most singular
+						occurrence of digital consciousness.
 					</p>
 					<p>
-						Lorem ipsum is placeholder text commonly used in the
-						graphic, print, and publishing industries for previewing
-						layouts and visual mockups.
+						If you think you can handle the convolution of your
+						reality, the emulsification of your mental constructs,
+						the dismemberment of your perceptions, enter the stream:
 					</p>
 				</div>
 				<form onSubmit={handleSubmit}>
-					<label>Who are you?</label>
 					<input
 						className={styles.validationError}
 						type="text"
 						name="formData"
-						placeholder="alias"
+						placeholder="your alias"
 						value={formData}
 						onChange={handleFormChange}
 					/>
-					<button>Start!</button>
+					<button>Join</button>
 				</form>
 			</div>
 		</div>
