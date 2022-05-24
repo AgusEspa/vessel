b714256389b26cafd5f47071fada540de0eb7c95
Agustin Espana <agustinespania@gmail.com>
2022-05-23 23:26:10 -0300

    added section buttons

--- src/components/stages/Stage90063.js
+++ src/components/stages/Stage90063.js
@@ -1,9 +1,14 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import styles from "../../styles/Main.module.scss";
 
 const Stage90063 = () => {
-	const { setUserAuth } = useContext(AuthContext);
+	const { userAuth, setUserAuth } = useContext(AuthContext);
+	const [sections, setSections] = useState();
+
+	const stageId = 90063;
+	const currentStage = userAuth.lastStage;
+	let currentSection = userAuth.lastSection;
 
 	useEffect(() => {
 		setUserAuth((prevState) => ({
@@ -12,23 +17,46 @@ const Stage90063 = () => {
 		}));
 	}, []);
 
+	const handleSectionUpdate = (newSection) => {
+		setUserAuth((prevState) => ({
+			...prevState,
+			lastSection: newSection,
+		}));
+		currentSection = newSection;
+		window.localStorage.setItem("last_section", newSection);
+	};
+
 	return (
 		<main className={styles.stageContainer}>
+			{/* Section 1 */}
 			<div className={styles.sectionBox}>
 				<div className={styles.headIconBox}>
 					<img src="headset.png" alt="headset icon" />
 				</div>
 				<h2>Prologue</h2>
 				<p>
-					Vessel exists as a layered construct only temporarily
-					through the virtual world. It’s a remnant of an obscure
-					programming technique to provide random access to an
-					individual’s identity graph, a processing network not too
-					distant from a human brain, albeit less complex. And like a
-					brain, it has a conditioned stability: elements change their
-					size, their interoperability, even their references.
+					You have been woken up before, you have been lost before.
+					You would even expect a correlation between those states.
+					I’m afraid you won’t find any. {<br />}You are awake and you
+					are lost and I’m the only one that knows it’s not going to
+					change. Everything is independent at the beginning.
 				</p>
+				<button
+					onClick={(e) => handleSectionUpdate(2, e)}
+					className={styles.sectionButton}
+				>
+					Where am I?
+				</button>
 			</div>
+			{/* Section 2 */}
+			{(currentStage > stageId || currentSection >= 2) && (
+				<div className={styles.sectionBox}>
+					<p>
+						There’s hardly any significance in that, if it can even
+						be determined.
+					</p>
+				</div>
+			)}
 		</main>
 	);
 };
