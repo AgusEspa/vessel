1b2cfa4b2e5ba230eb66eab13fca500e9f11722a
Agustin Espana <agustinespania@gmail.com>
2022-06-02 17:20:57 -0300

    fixes

--- src/components/FreshLanding.js
+++ src/components/FreshLanding.js
@@ -1,6 +1,7 @@
 import { useState, useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { useNavigate, Link } from "react-router-dom";
+import { IoInfiniteOutline } from "react-icons/io5";
 import Navbar from "./Navbar";
 import styles from "../styles/Main.module.scss";
 
@@ -50,36 +51,40 @@ const FreshLanding = (props) => {
 
 				<div className={styles.welcomeContainer}>
 					<div>
-						<div className={styles.dots}>
-							<span></span>
-							<span></span>
-							<span></span>
-						</div>
-						<h1>is never what it might seem</h1>
-						<div className={styles.dots}>
-							<span></span>
-							<span></span>
-							<span></span>
+						<p className={styles.quotePhrase}>
+							“the eternal mystery of the world
+							<br /> is its comprehensibility”
+						</p>
+						<p className={styles.quoteAuthor}>
+							Albert Einstein, <span>Physics and Reality</span>
+						</p>
+						<div className={styles.separator}>
+							<IoInfiniteOutline />
 						</div>
 						<p>
-							but you will try to make sense out of it regardless.
+							Humans have often believed they’ve comprehended more
+							than they’ve been mystified, and that might end up
+							undermining what they take for granted. For some it
+							is already too late, for others, perhaps including
+							yourself, the world is still within reach.
 						</p>
-						<h1>
+						<p className={styles.quotePhrase}>
 							<span className={styles.colored}>
 								Is this a game?
 							</span>
-						</h1>
-						<p>
-							Isn't everything? This is also the opposite. Try to
-							quantify your brain’s ability to change and you will
-							understand.
 						</p>
-
+						<p>Isn't everything? This is also the opposite.</p>
 						<p>
-							If then you feel prepared to handle the convolution
-							of reality, the dismemberment of your perceptions,
-							the dissolution of your mental patterns, enter the
-							stream:
+							There are multiple implementations for the idea
+							conveyed by ‘vessel’, some imply the act of
+							creation, others the act of destruction. However,
+							what seems opposing might as well be symbiotic.
+						</p>
+						<p>
+							If you feel prepared to handle the mystification of
+							reality, the dismemberment of your perceptions, the
+							dissolution of your mental patterns, enter the
+							frame:
 						</p>
 					</div>
 					<form onSubmit={handleSubmit}>
