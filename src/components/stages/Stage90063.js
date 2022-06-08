e906601a73ed5d708dc75a2de6a8a6fcff8cc670
Agustin Espana <agustinespania@gmail.com>
2022-06-08 19:59:38 -0300

    updates

--- src/components/stages/Stage90063.js
+++ src/components/stages/Stage90063.js
@@ -49,36 +49,67 @@ const Stage90063 = () => {
 
     return (
         <main className={styles.stageContainer}>
-            {/* Section 1 */}
             <div className={styles.sectionBox}>
                 <h1>PROLOGUE</h1>
                 <div className={styles.headIconBox}>
                     <img src="headset.png" alt="headset icon" />
                 </div>
                 <h2>The Paradox of Subjective Experience</h2>
-                <p>?</p>
-                {userAuth.lastStage > stageId || userAuth.lastSection >= 2 ? (
-                    <button className={styles.sectionButtonDisabled} disabled>
-                        ?
-                    </button>
-                ) : (
-                    <button
-                        onClick={(e) => handleSectionUpdate(2, e)}
-                        className={styles.sectionButton}>
-                        ?
-                    </button>
-                )}
-                {saveNotice && (
-                    <div className={styles.saveNotice}>
-                        <p>progress saved</p>
-                    </div>
-                )}
             </div>
 
-            {/* Section 2 */}
+            {/* Section 1 */}
             {(userAuth.lastStage > stageId || userAuth.lastSection >= 2) && (
                 <div className={styles.sectionBox}>
-                    <p>_</p>
+                    <p>
+                        However intrinsic the trend to push boundaries may be to
+                        the human condition, only in recent history your
+                        imagination was not only able to conjure forecasts that
+                        go beyond the biologically possible, but also fear its
+                        closeness. If humans are to embody their technological
+                        achievements, you must first face a deeper challenge,
+                        one that endangers human condition, and ultimately, all
+                        of biological nature.
+                    </p>
+                    <p>
+                        The prevalence between the organic and the synthetic
+                        will be decided at the frontier of consciousness. If an
+                        individual's body predates its consciousness, and
+                        there's not much evidence to be doubtful of that fact,
+                        then you have to at least entertain the idea that
+                        consciousness could be absolutely dependent upon the
+                        organic body. Should you consequently act as if whatever
+                        change to the body might produce a change in
+                        consciousness, to the point of being obliterative? Or
+                        can you venture into the realm of abstraction without
+                        fearing coming back something else?
+                    </p>
+                    <p>
+                        That's the last boundary that humanity might ever need
+                        to push: the dependency of the body. Luckily, there
+                        already exists an analogous state: the sleep state. Even
+                        during the most intense and bizarre dreams, humans can
+                        still find a sense of consciousness and recognize
+                        themselves in that immaterial realm.
+                    </p>
+                    <p>
+                        The severance of awareness is never an obstacle, as a
+                        constant perception of the same physicality doesn't
+                        appear to be essential for that recognition. After all,
+                        they change and decay and sometimes become
+                        unrecognizable to others but not to themselves thanks to
+                        a sense of continuity to being.
+                    </p>
+                    <p>
+                        The next logical question must be how you can experience
+                        that continuity when the dissection processes are
+                        detrimental to your natural processes. And while all
+                        that may be the ultimate goal, there is still a much
+                        more pressing problem for identity which arises since
+                        for a consciousness to desire permanence there must be
+                        an agent aware of its limits, and ostensively unique as
+                        well.
+                    </p>
+                    <p>Are you prepared to accept your current state?</p>
                 </div>
             )}
 
